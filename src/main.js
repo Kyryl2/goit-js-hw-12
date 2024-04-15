1b30f52617a990c73ea4522dbea832479d87b2c6
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-15 16:40:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,36 +15,16 @@ let page = 1;
 const limit = 15;
 let search = 0;
 
-// const totalPages = Math.ceil(quantity.data.totalHits / limit); // Чи вірно це?
-const totalPages = Math.ceil(1000 / limit);
+const options = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+const imageModal = new SimpleLightbox('.gallery a', options);
+
 btn.classList.add('is-hidden');
 
 btn.addEventListener('click', morePosts);
 form.addEventListener('submit', onClickBtn);
-
-async function morePosts() {
-  try {
-    page += 1;
-    btn.classList.add('is-hidden');
-    loader.classList.remove('is-hidden');
-    // search = input.value.trim(); // Чому якщо прибрати змінну search з функції і залишити в глобальному, то з другого кліка вже не знаходить значення інпута, а бачить порожній інпут???
-
-    const { data } = await getPhotos(search, page, limit);
-    if (page === Math.ceil(data.totalHits / 15)) {
-      btn.classList.add('is-hidden');
-      loader.classList.add('is-hidden');
-      return iziToast.error({
-        position: 'topRight',
-        message: "We're sorry, but you've reached the end of search results.",
-      });
-    }
-    list.insertAdjacentHTML('beforeend', createGallaryMarkup(data.hits));
-    btn.classList.remove('is-hidden');
-    loader.classList.add('is-hidden');
-  } catch (error) {
-    console.log(error);
-  }
-}
 
 async function onClickBtn(event) {
   event.preventDefault();
@@ -58,10 +38,10 @@ async function onClickBtn(event) {
     event.target.reset();
 
     return iziToast.error({
-      message: 'Поле для введення не має бути порожнім!',
       position: 'topRight',
-      timeout: 2000,
-      color: 'yellow',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      maxWidth: 300,
     });
   }
   list.innerHTML = '';
@@ -69,29 +49,89 @@ async function onClickBtn(event) {
     loader.classList.remove('is-hidden');
     btn.classList.add('is-hidden');
     const lists = await getPhotos(search, 1);
+    console.log(lists);
     if (lists.data.hits.length === 0) {
       list.innerHTML = '';
       loader.classList.add('is-hidden');
       btn.classList.add('is-hidden');
       form.reset();
 
-      iziToast.error({
-        message: 'За вашим пошуковим словом, зображень не знайдено!',
+      return iziToast.error({
         position: 'topRight',
-        timeout: 2000,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        maxWidth: 300,
+      });
+    }
+    if (lists.data.total <= limit) {
+      list.insertAdjacentHTML(
+        'beforeend',
+        createGallaryMarkup(lists.data.hits)
+      );
+      btn.classList.add('is-hidden');
+      loader.classList.add('is-hidden');
+      btn.removeEventListener('click', onClickBtn);
+      iziToast.warning({
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+        color: 'yellow',
       });
       return;
     }
-
     list.insertAdjacentHTML('beforeend', createGallaryMarkup(lists.data.hits));
     btn.classList.remove('is-hidden');
     loader.classList.add('is-hidden');
-    const options = {
-      captionsData: 'alt',
-      captionDelay: 250,
-    };
-    const imageModal = new SimpleLightbox('.gallery a', options);
+
     imageModal.refresh();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function morePosts() {
+  page += 1;
+  btn.classList.add('is-hidden');
+  loader.classList.remove('is-hidden');
+  try {
+    // search = input.value.trim(); // Чому якщо прибрати змінну search з функції і залишити в глобальному, то з другого кліка вже не знаходить значення інпута, а бачить порожній інпут???
+
+    const { data } = await getPhotos(search, page, limit);
+    const lastPage = Math.ceil(data.total / limit);
+    if (page === lastPage) {
+      btn.classList.add('is-hidden');
+      loader.classList.add('is-hidden');
+      console.log(page);
+      console.log(lastPage);
+      list.insertAdjacentHTML('beforeend', createGallaryMarkup(data.hits));
+      iziToast.warning({
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+        color: 'yellow',
+      });
+      imageModal.refresh();
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+      return;
+    }
+    list.insertAdjacentHTML('beforeend', createGallaryMarkup(data.hits));
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    imageModal.refresh();
+    btn.classList.remove('is-hidden');
+    loader.classList.add('is-hidden');
   } catch (error) {
     console.log(error);
   }
